34096c6a35c8b3ec003b700f47a28face36cba3d
likvidov <likvidya@gmail.com>
2020-04-01 15:40:40 +0300

    Updates

---
+++ precache-manifest.50896b015d2e3b2f8b969c6e9a836d3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981691d99716ba8acc927a90c95ba1c6",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "f7753549e3fe20f77bdc",
+    "url": "/movie/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cfdacb8727918375935e",
+    "url": "/movie/static/css/main.cab8b1b3.chunk.css"
+  },
+  {
+    "revision": "f7753549e3fe20f77bdc",
+    "url": "/movie/static/js/2.3aece274.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie/static/js/2.3aece274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfdacb8727918375935e",
+    "url": "/movie/static/js/main.120bfb21.chunk.js"
+  },
+  {
+    "revision": "ff18b1477b07fc611b66",
+    "url": "/movie/static/js/runtime-main.3a6b4689.js"
+  }
+]);
